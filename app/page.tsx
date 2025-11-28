a2eb0d0e5ed4df939652eb1dc3a87b02d6d75a73
Rizel Scarlett <rizel@squareup.com>
2025-11-28 15:46:57 -0500

    Fix darker blue radial gradient on right side - increase lightness from 15% to 25%

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-background relative">
       {/* Subtle background pattern */}
-      <div className="absolute inset-0 bg-[radial-gradient(circle_at_30%_20%,_hsl(210_60%_15%),_transparent_50%),_radial-gradient(circle_at_70%_80%,_hsl(190_80%_15%),_transparent_50%)] opacity-40" />
+      <div className="absolute inset-0 bg-[radial-gradient(circle_at_30%_20%,_hsl(210_60%_15%),_transparent_50%),_radial-gradient(circle_at_70%_80%,_hsl(190_80%_25%),_transparent_50%)] opacity-40" />
       
       <div className="relative z-10">
         <Hero />
